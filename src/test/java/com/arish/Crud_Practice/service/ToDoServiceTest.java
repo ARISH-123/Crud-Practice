77d49cba043f26102a4656684bc22d039b05a387
Arish Ahmad <arish.ahmad1999@gmail.com>
2026-08-27 22:50:33 +0530

    Created unit test for testGetToDo()

--- src/test/java/com/arish/Crud_Practice/service/ToDoServiceTest.java
+++ src/test/java/com/arish/Crud_Practice/service/ToDoServiceTest.java
@@ -3,10 +3,19 @@ package com.arish.Crud_Practice.service;
 
 import com.arish.Crud_Practice.Repository.ToDoRepository;
 import com.arish.Crud_Practice.model.ToDo;
+import com.arish.Crud_Practice.model.ToDoUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ToDoServiceTest {
@@ -16,4 +25,26 @@ public class ToDoServiceTest {
     private ToDoService toDoService;
 
     private ToDo todo;
+
+
+    @BeforeEach
+    void setUp() {
+        todo = new ToDo(
+                1,
+                "Todo1",
+                "description of todo 1"
+        );
+
+
+    }
+
+    @Test
+    void testGetToDo(){
+        int id=1;
+        when (toDoRepository.findById(id)).thenReturn(Optional.of(todo));
+        ToDo currentToDo = toDoService.getTodoById(id);
+        assertNotNull(currentToDo);
+        assertEquals("Todo1", currentToDo.getTitle());
+        verify(toDoRepository, times(1)).findById(id);
+    }
 }
